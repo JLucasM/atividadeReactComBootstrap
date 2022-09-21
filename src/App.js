f725c7e9923628a6e489dc42f2c36ecdb5a992ec
JLucasM <menezes.joaolucas@gmail.com>
2022-09-21 00:59:16 -0300

    Enviando arquivos modificados

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card className='mb-3' style={{ color: "#000", backgroundColor: "#7D8C65" }}>
+          <Card.Img src="http://cdn.simplesite.com/i/3b/2d/283726781727452475/i283726789582726502.jpg" />
+          <Card.Body>
+            <Card.Title style={{ fontFamily: "fantasy" }}>
+              Teste de aceitação
+            </Card.Title>
+            <Form>
+              <Form.Group className='mb-4 mt-4'>
+                <Form.Control className='mb-2' type="number" placeholder='Quantos zumbis você já matou?' />
+                <Form.Control className='mb-2' type="number" placeholder='Quantas pessoas você já matou?' />
+                <Form.Control className='mb-2' type="text" placeholder='Por que?' />
+              </Form.Group>
+            </Form>
+            <Button variant='danger'>Reprovado</Button>
+            <Button className='mx-2' variant='success'>Aprovado</Button>
+          </Card.Body>
+
+        </Card>
+
       </header>
     </div>
   );
